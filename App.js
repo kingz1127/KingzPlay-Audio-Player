0dfc1405febb8367a0934b89bad3b608618dd577
kingz1127 <osunyingboadedeji1@gmail.com>
2026-02-21 18:34:23 +0100

    drawer

--- App.js
+++ App.js
@@ -1,97 +1,12 @@
-// import React from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import { createDrawerNavigator } from "@react-navigation/drawer";
-// import { TouchableOpacity } from "react-native";
-// import { Ionicons } from "@expo/vector-icons";
-// import HomeScreen from "./screens/HomeSreen";
-// import PlayerScreen from "./screens/PlayerSreen";
-// import DownloadListScreen from "./screens/DownloadListSreeen";
-// import LocalTracksListScreen from "./screens/LocalTracksListSreen";
-// import FavouriteScreen from "./screens/FavouriteScreen";
-
-// import { AudioProvider } from "./context/AudioContext";
-
-// const Stack = createNativeStackNavigator();
-// const Drawer = createDrawerNavigator();
-
-
-// function DrawerNavigator() {
-//   return (
-//     <Drawer.Navigator
-//       screenOptions={({ navigation }) => ({
-        
-//         headerLeft: () => (
-//           <TouchableOpacity
-//             onPress={() => navigation.toggleDrawer()}
-//             style={{ marginLeft: 15 }}
-//           >
-//             <Ionicons name="menu" size={25} color="black" />
-//           </TouchableOpacity>
-//         ),
-//       })}
-//     >
-//       <Drawer.Screen name="Home" component={PlayerScreen} />
-//       <Drawer.Screen name="Downloads" component={DownloadListScreen} />
-//       <Drawer.Screen name="Favourites" component={FavouriteScreen} />
-//       <Drawer.Screen name="Local Files" component={LocalTracksListScreen} />
-//     </Drawer.Navigator>
-
-    
-//   );
-// }
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator screenOptions={{ headerShown: false }}>
-//         <Stack.Screen name="HomeScreen" component={HomeScreen} />
-//         <Stack.Screen name="PlayerScreen" component={DrawerNavigator} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-
 
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { TouchableOpacity } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
 import HomeScreen from "./screens/HomeSreen";
-import PlayerScreen from "./screens/PlayerSreen";
-import DownloadListScreen from "./screens/DownloadListSreeen";
-import LocalTracksListScreen from "./screens/LocalTracksListSreen";
-import FavouriteScreen from "./screens/FavouriteScreen";
-
+import PlayerScreen from "./screens/PlayerSreen"; 
 import { AudioProvider } from "./context/AudioContext";
 
 const Stack = createNativeStackNavigator();
-const Drawer = createDrawerNavigator();
-
-function DrawerNavigator() {
-  return (
-    <Drawer.Navigator
-      screenOptions={({ navigation }) => ({
-        headerLeft: () => (
-          <TouchableOpacity
-            onPress={() => navigation.toggleDrawer()}
-            style={{ marginLeft: 15 }}
-          >
-            <Ionicons name="menu" size={25} color="black" />
-          </TouchableOpacity>
-        ),
-      })}
-    >
-      <Drawer.Screen name="Home" component={PlayerScreen} />
-      <Drawer.Screen name="Downloads" component={DownloadListScreen} />
-      <Drawer.Screen name="Favourites" component={FavouriteScreen} />
-      <Drawer.Screen name="Local Files" component={LocalTracksListScreen} />
-    </Drawer.Navigator>
-  );
-}
 
 export default function App() {
   return (
@@ -99,7 +14,7 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name="HomeScreen" component={HomeScreen} />
-          <Stack.Screen name="PlayerScreen" component={DrawerNavigator} />
+          <Stack.Screen name="PlayerScreen" component={PlayerScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </AudioProvider>
--- screens/DownloadListSreeen.js
+++ screens/DownloadListSreeen.js
@@ -1,8 +1,12 @@
 import { Text, View } from "react-native";
+import CustomHeader from "../components/ui/CustomHeader";
 
 export default function DownloadListScreen(){
     return(
         <View>
+            <View>
+      <CustomHeader title="Downloads" />
+    </View>
             <Text>this is file manager audio</Text>
         </View>
     )
